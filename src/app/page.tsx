46cf755ea654f7bd72181c347af72c54a59ea8fa
mark-elias <melias23@proton.me>
2026-03-23 05:40:31 -0700

    basic country selection for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,50 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+// zustand country store
+import { useCountryStore } from "../lib/store/useCountryStore";
+
 function Home() {
+  // zustand
+  const selectedCountry = useCountryStore((state) => state.selectedCountry);
+  const setCountry = useCountryStore((state) => state.setCountry);
+  // router
+  const router = useRouter;
+
+  const handleCountrySelect = (country: "MX" | "CA") => {
+    setCountry(country);
+  };
+
+  const TEXT = {
+    title: "Select Country",
+    description: "select the country you are traveling from",
+    mexico: "Mexico",
+    canada: "Canada",
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-4">EasyBorder</h1>
-      <p className="text-lg text-gray-600">Frontend initial setup</p>
+    <main className="flex flex-col items-center pt-30 min-h-screen">
+      <h1 className="text-3xl font-bold">{TEXT.title}</h1>
+      <p className="text-xl text-secondary-grey">{TEXT.description}</p>
+      <div className="mt-20 flex flex-col gap-10">
+        <button
+          onClick={() => handleCountrySelect("MX")}
+          className="text-4xl border-secondary-grey border-1 py-5 px-10 flex gap-2 justify-center rounded-xl hover:bg-primary-blue hover:border-primary-blue "
+        >
+          <span>🇲🇽</span>
+          {TEXT.mexico}
+        </button>
+        <button
+          onClick={() => handleCountrySelect("CA")}
+          className="text-4xl border-secondary-grey border-1 py-5 px-10 flex gap-2 justify-center rounded-xl hover:bg-primary-blue hover:border-primary-blue shadow-3xl "
+        >
+          <span>🇨🇦</span>
+          {TEXT.canada}
+        </button>
+      </div>
+      <div className="mt-30">
+        <p>Selected Country: {selectedCountry}</p>
+      </div>
     </main>
   );
 }
