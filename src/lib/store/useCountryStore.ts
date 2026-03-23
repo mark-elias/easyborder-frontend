95498f2c91a0e7d89d917c222dfcec2a2b626979
mark-elias <melias23@proton.me>
2026-03-23 03:45:14 -0700

    create country store

---
+++ src/lib/store/useCountryStore.ts
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+
+// interface for my store
+interface CountryStore {
+  selectedCountry: "MX" | "CA" | null;
+  selectedCity: string | null;
+
+  // actions
+  setCountry: (country: "MX" | "CA") => void;
+  setCity: (city: string) => void;
+  reset: () => void;
+}
+
+// create my store
+// with my type/interface
+export const useCountryStore = create<CountryStore>(
+  // function that defines the store
+  (set) => ({
+    // initial state
+    selectedCountry: null,
+    selectedCity: null,
+
+    // actions
+    // to update the country and reset city
+    setCountry: (country) =>
+      set({
+        selectedCountry: country,
+        // reset city if new country is selected
+        selectedCity: null,
+      }),
+
+    // to update just the city
+    setCity: (city) => set({ selectedCity: city }),
+
+    // for clearing country and city
+    reset: () =>
+      set({
+        selectedCountry: null,
+        selectedCity: null,
+      }),
+  }),
+);
