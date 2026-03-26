e469f34c0d110280499df6c0333faf7872ea8af6
mark-elias <melias23@proton.me>
2026-03-26 03:59:26 -0700

    basic cities page

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+// zustand
+import { useCountryStore } from "@/src/lib/store/useCountryStore";
 
-function page() {
+// mock data
+const MOCK_CITIES = {
+  MX: ["Tijuana", "Nogales", "Ciudad Juarez", "Mexicali", "Nuevo Laredo"],
+  CA: ["Vancouver", "Toronto", "Montreal", "Calgary"],
+};
+
+function CitiesPage() {
+  const selectedCountry = useCountryStore((state) => state.selectedCountry);
+
+  if (!selectedCountry) {
+    return <div>Please select a country first</div>;
+  }
+
+  const cities = MOCK_CITIES[selectedCountry] || ["city1", "city2", "city3"];
+
   return (
-    <div>page</div>
-  )
+    <main className="min-h-screen p-5">
+      <h1 className="text-4xl font-bold mb-8">Select City</h1>
+      <h3>select the city you are crossing from</h3>
+
+      <div className="flex flex-col gap-5 mt-10">
+        {cities.map((city) => (
+          <div key={city} className="border-1 border-white bg-primary p-5 rounded-2xl">
+            {city}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
-export default page
+export default CitiesPage;
