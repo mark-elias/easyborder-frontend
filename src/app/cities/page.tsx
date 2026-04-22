cadb5927a68ef4374dd6e73d960bae83d0a4a995
mark-elias <melias23@proton.me>
2026-04-22 06:49:17 -0700

    add change country button for citites page

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -12,35 +12,51 @@ import { Button } from "@/components/ui/button";
 import { useRequireCountry } from "@/src/hooks/useRequireCountry";
 
 function CitiesPage() {
+  const router = useRouter();
   // my custom hook; returns selected country value
   // and redirects to homepage if country is NOT selected
   const selectedCountry = useRequireCountry();
   //zustand
   const setCity = useCountryAndCityStore((state) => state.setCity);
+  const reset = useCountryAndCityStore((state) => state.reset);
 
   if (!selectedCountry) return null;
 
   const cities = selectedCountry === "MX" ? MEXICO_CITIES : CANADA_CITIES;
 
-  return (
-    <main className="flex flex-col items-center gap-10 min-h-screen p-5 my-10 lg:mt-20">
-      <div className="text-center">
-        <h1>{CITIES_PAGE_TEXT.title}</h1>
-        <h3 className="text-custom-grey">{CITIES_PAGE_TEXT.description}</h3>
-      </div>
+  const handleChangeCountry = () => {
+    reset();
+    router.push("/");
+  };
 
-      <section className="flex flex-col lg:flex-row lg:flex-wrap gap-5 lg:px-60">
-        {cities.map((city) => (
-          <Button
-            key={city}
-            className="text-lg p-6 border-custom-blue hover:bg-custom-blue"
-            variant="outline"
-            onClick={() => setCity(city)}
-          >
-            {city}
-          </Button>
-        ))}
-      </section>
+  return (
+    <main className="min-h-screen p-5">
+      <Button
+        onClick={handleChangeCountry}
+        variant="default"
+        className="p-4 hover:cursor-pointer"
+      >
+        Change Country
+      </Button>
+
+      <div className="flex flex-col items-center gap-10 my-10">
+        <div className="text-center">
+          <h1>{CITIES_PAGE_TEXT.title}</h1>
+          <h3 className="text-custom-grey">{CITIES_PAGE_TEXT.description}</h3>
+        </div>
+        <section className="flex flex-col lg:flex-row lg:flex-wrap gap-5 lg:px-60">
+          {cities.map((city) => (
+            <Button
+              key={city}
+              className="text-lg p-6 border-custom-blue hover:bg-custom-blue"
+              variant="outline"
+              onClick={() => setCity(city)}
+            >
+              {city}
+            </Button>
+          ))}
+        </section>
+      </div>
     </main>
   );
 }
