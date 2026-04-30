58e7561ed51d9f8d1fbd54179c6578f164c18da6
mark-elias <melias23@proton.me>
2026-04-30 07:12:42 -0700

    revert back to regular crossing page

--- src/app/crossings/page.tsx
+++ src/app/crossings/page.tsx
@@ -12,30 +12,23 @@ function CrossingsPage() {
     error,
   } = useCrossings(selectedCountry, selectedCity);
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <h3 className="text-center mt-10 bg-custom-yellow text-black p-2 mx-10 rounded">
-      Im currently setting up nginx, docker-compose, and SSL for https... stay
-      tuned
-    </h3>
+    <main className="p-5 min-h-screen">
+      <section className="flex flex-col items-center justify-center border">
+        <h3>Crossings for {selectedCity}</h3>
+        <p>{selectedCountry}</p>
+        <p>{selectedCity}</p>
+        <ul className="">
+          {crossings?.map((crossing) => (
+            <li key={crossing._id}>{crossing.portName}</li>
+          ))}
+        </ul>
+      </section>
+    </main>
   );
-
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error.message}</div>;
-
-  // return (
-  //   <main className="p-5 min-h-screen">
-  //     <section className="flex flex-col items-center justify-center border">
-  //       <h3>Crossings for {selectedCity}</h3>
-  //       <p>{selectedCountry}</p>
-  //       <p>{selectedCity}</p>
-  //       <ul className="">
-  //         {crossings?.map((crossing) => (
-  //           <li key={crossing._id}>{crossing.portName}</li>
-  //         ))}
-  //       </ul>
-  //     </section>
-  //   </main>
-  // );
 }
 
 export default CrossingsPage;
