50e96ffc88983f34ec068da45d16c3a01e978e97
mark-elias <melias23@proton.me>
2026-05-15 03:32:45 -0700

    ui updates for Crossing cards, page

--- src/app/crossings/page.tsx
+++ src/app/crossings/page.tsx
@@ -18,6 +18,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { BadgeCheck } from "lucide-react";
+
 // components
 import { LoadingSpinnerWithText } from "@/src/components/molecules";
 
@@ -51,7 +53,7 @@ function CrossingsPage() {
         {CROSSINGS_PAGE_TEXT.changeCityButtonText}
       </Button>
       <section className="flex flex-col items-center justify-center">
-        <div className="text-center mt-5 mb-14">
+        <div className="text-center mt-5">
           <h1>
             {CROSSINGS_PAGE_TEXT.title}
             {selectedCity}
@@ -60,20 +62,30 @@ function CrossingsPage() {
             {CROSSINGS_PAGE_TEXT.description}
           </h3>
         </div>
+        <div className="flex gap-1 mt-10 mb-5 text-sm items-center text-custom-green">
+          <BadgeCheck className="size-3.5" />
+          <p>Official CBP data</p>
+        </div>
         <div className="flex flex-wrap gap-10">
           {crossings?.map((crossing) => (
             <Card
               key={crossing._id}
-              className="min-w-[375px] max-w-[400px] border border-custom-blue"
+              className="w-[350px] border border-custom-blue 
+              shadow-lg
+              hover:cursor-pointer hover:scale-[1.03]
+              transition-transform duration-200 ease-in-out"
             >
-              <CardHeader className="flex items-baseline gap-2">
-                <CardTitle className="text-2xl">{crossing.portName}</CardTitle>
-                <CardDescription className="text-xl font-semibold">
+              <CardHeader>
+                <div className="flex justify-between items-baseline">
+                  <CardTitle className="text-xl">{crossing.portName}</CardTitle>
+                  <CardDescription>Port #{crossing.portNumber}</CardDescription>
+                </div>
+                <CardDescription className="text-lg font-semibold">
                   {crossing.crossingName}
                 </CardDescription>
               </CardHeader>
 
-              <CardContent className="flex flex-col gap-2 text-lg font-semibold my-5">
+              <CardContent className="flex flex-col gap-2 font-semibold mt-3">
                 <p>
                   {CROSSINGS_PAGE_TEXT.portStatusText}
                   <span
@@ -91,16 +103,6 @@ function CrossingsPage() {
                 <p>
                   {CROSSINGS_PAGE_TEXT.hoursOfOperationText}
                   <span className="font-normal">{crossing.hours}</span>
-                </p>
-                <p>
-                  {CROSSINGS_PAGE_TEXT.portNumberText}
-                  <span className="font-normal">{crossing.portNumber}</span>
-                </p>
-                <p>
-                  {CROSSINGS_PAGE_TEXT.lastUpdateText}
-                  <span className="font-normal">
-                    {crossing.date} {crossing.time}
-                  </span>
                 </p>
               </CardContent>
 
