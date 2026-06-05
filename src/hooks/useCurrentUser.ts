1cbbccfd591556eabaee75497362135637e8419c
mark-elias <melias23@proton.me>
2026-06-04 21:51:06 -0700

    refactor: simplify useCurrentUser file

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,27 +1,10 @@
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
-export interface CurrentUser {
-  _id: string;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-const API_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001/api";
+import { authService } from "../services/authService";
 
 function useCurrentUser() {
-  const fetchCurrentUser = () =>
-    axios
-      .get<{ user: CurrentUser }>(`${API_URL}/auth/me`, {
-        withCredentials: true,
-      })
-      .then((res) => res.data.user);
-
   return useQuery({
     queryKey: ["currentUser"],
-    queryFn: fetchCurrentUser,
+    queryFn: authService.me,
     retry: false,
   });
 }
