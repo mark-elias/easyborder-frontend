32fab74c77404f51120a688e0b4bbafcd7dfdad6
mark-elias <melias23@proton.me>
2026-06-05 07:13:45 -0700

    refactor: cleanup waittimes hook

--- src/hooks/useWaitTimes.ts
+++ src/hooks/useWaitTimes.ts
@@ -1,20 +1,11 @@
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-// interfaces/types
-import { WaitTime } from "../types/types";
-
-const API_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001/api";
+import { waitTimesService } from "@/src/services/waitTimesService";
 
 function useWaitTimes(crossingId: string) {
-  const fetchWaitTimes = () =>
-    axios
-      .get<WaitTime>(`${API_URL}/wait-times/${crossingId}`)
-      .then((res) => res.data);
-
   return useQuery({
     queryKey: ["waitTimes", crossingId],
-    queryFn: fetchWaitTimes,
+    queryFn: () => waitTimesService.getWaitTimes(crossingId),
+    enabled: !!crossingId,
   });
 }
 
