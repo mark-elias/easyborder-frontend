cb9c84fd4d682bf7c86af03b5f4e35beab588722
mark-elias <melias23@proton.me>
2026-07-02 03:34:12 -0700

    create WaitTimeLaneCard molecule

---
+++ src/components/molecules/WaitTimeLaneCard/WaitTimeLaneCard.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Heart } from "lucide-react";
+import type { FavoriteLaneType, LaneDetail, TravelerType } from "@/src/types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TravelerTypeIcon } from "@/src/components/atoms";
+import {
+  getWaitTimeColor,
+  LANE_LABELS,
+  TRAVELER_TYPE_LABELS,
+} from "@/src/lib/utils/waitTimeUtils";
+
+interface Props {
+  travelerType: TravelerType;
+  laneType: FavoriteLaneType;
+  data: LaneDetail;
+  isFavorited: boolean;
+  onHeartClick: () => void;
+}
+
+export function WaitTimeLaneCard({
+  travelerType,
+  laneType,
+  data,
+  isFavorited,
+  onHeartClick,
+}: Props) {
+  return (
+    <Card
+      className="w-[300px] shadow-lg hover:cursor-pointer hover:scale-[1.03]
+      transition-transform duration-200 ease-in-out"
+    >
+      <CardHeader>
+        <div className="flex justify-between">
+          <div className="flex gap-2 items-center text-custom-grey">
+            <TravelerTypeIcon travelerType={travelerType} className="size-6" />
+            <CardDescription className="text-lg font-semibold">
+              {TRAVELER_TYPE_LABELS[travelerType]}
+            </CardDescription>
+          </div>
+          <Heart
+            onClick={onHeartClick}
+            className={`hover:cursor-pointer transition-colors ${
+              isFavorited ? "text-red-500 fill-red-500" : "hover:text-red-500"
+            }`}
+          />
+        </div>
+        <CardTitle className="font-semibold text-2xl">
+          {LANE_LABELS[laneType]}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="text-3xl font-bold text-center my-3">
+        <p className={getWaitTimeColor(data.delayMinutes || 0)}>
+          {data.delayMinutes} mins
+        </p>
+      </CardContent>
+      <CardFooter className="border-0 flex flex-col items-start">
+        <p>Lanes Open: {data.lanesOpen}</p>
+        <p>Operational Status: {data.operationalStatus}</p>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,10 +2,12 @@ import CountryButton from "./CountryButton/CountryButton";
 import LoadingSpinnerWithText from "./LoadingSpinnerWithText/LoadingSpinnerWithText";
 import FavoriteLaneCard from "./FavoriteLaneCard/FavoriteLaneCard";
 import { FavoritesEmptyState } from "./FavoritesEmptyState/FavoritesEmptyState";
+import { WaitTimeLaneCard } from "./WaitTimeLaneCard/WaitTimeLaneCard";
 
 export {
   CountryButton,
   LoadingSpinnerWithText,
   FavoriteLaneCard,
   FavoritesEmptyState,
+  WaitTimeLaneCard,
 };
